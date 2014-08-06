d5211184311245dae06ecb7d017daa3e06926359
dylan.huang <dylan.huang@ischool.com.tw>
2014-08-06 12:10:01 +0800

    實驗中學_雙語部_成績系統_學生修課資料項目(0806)

---
+++ Student_CourseItem/Permissions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BasicInformation.Student_CourseItem
+{
+    class Permissions
+    {
+        public static string 學生修課 { get { return "JHSchool.Student.Detail0052"; } }
+        public static bool 學生修課權限
+        {
+            get
+            {
+                return FISCA.Permission.UserAcl.Current[學生修課].Executable;
+            }
+        }
+    }
+}
---
+++ Student_CourseItem/Program.cs
@@ -0,0 +1,25 @@
+﻿using FISCA;
+using FISCA.Permission;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicInformation.Student_CourseItem
+{
+    public class Program
+    {
+        [MainMethod()]
+        static public void Main()
+        {
+            FeatureAce UserPermission = FISCA.Permission.UserAcl.Current[Permissions.學生修課];
+            if (UserPermission.Editable || UserPermission.Viewable)
+                K12.Presentation.NLDPanels.Student.AddDetailBulider(new FISCA.Presentation.DetailBulider<CourseScoreItem>());
+
+
+            FISCA.Permission.Catalog detail1 = RoleAclSource.Instance["學生"]["資料項目"];
+            detail1.Add(new DetailItemFeature(Permissions.學生修課, "學生修課"));
+        }
+    }
+}
